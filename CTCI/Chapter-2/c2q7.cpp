002f5b527a28c9aaf7a4f3a4ac7bc36b2d6f11cd
Luke Volpatti <luke.volpatti@gmail.com>
2017-12-27 18:07:24 -0500

    c2q7

---
+++ CTCI/Chapter-2/c2q7.cpp
@@ -0,0 +1,40 @@
+// Intersection
+#include "LinkedList.h"
+#include <unordered_map>
+
+Node* intersection(Node* l1, Node* l2){
+	Node* t1 = l1;
+	Node* t2 = l2;
+	unordered_map<Node*, bool> m;
+
+	while(t1){
+		m[t1] = true;
+		t1 = t1->next;
+	}
+
+	while(t2){
+		if(m[t2]) return t2;
+		t2 = t2->next;
+	}
+
+	return NULL;
+}
+
+int main(){
+	// Some testing
+	Node* N1 = new Node;
+	Node* N2 = new Node;
+	Node* N3 = new Node;
+	Node* N4 = new Node;
+	N1->data = 1;
+	N2->data = 2;
+	N3->data = 3;
+	N4->data = 4;
+	N1->next = N2;
+	N2->next = N3;
+	N3->next = NULL;
+	N4->next = N2;
+	Node* head = N1;
+	Node* head2 = N4;
+	cout << intersection(head, head2) << endl;
+}
