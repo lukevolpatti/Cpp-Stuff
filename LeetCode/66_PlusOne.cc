cf9aebc4c15540903b2af50a0cacbf3f1bc0cbbf
Luke Volpatti <luke.volpatti@gmail.com>
2018-08-21 17:15:51 -0400

    plus one

---
+++ LeetCode/66_PlusOne.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> plusOne(vector<int>& digits) {
+    int i = digits.size() - 1;
+    while(i >= 0 && digits[i] == 9){
+        digits[i] = 0;
+        i--;
+    }
+    if(i == -1) {
+        digits[0] = 1;
+        digits.push_back(0);
+    }
+    else digits[i]++;
+    return digits;
+}
+
+int main(){
+	
+}
