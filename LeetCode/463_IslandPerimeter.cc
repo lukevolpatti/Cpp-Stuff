184a5829709c2d0b6775d187f0d458ac36cae831
Luke Volpatti <luke.volpatti@gmail.com>
2018-09-05 20:24:31 -0400

    robot return to origin

--- LeetCode/463_IslandPerimeter.cc
+++ LeetCode/463_IslandPerimeter.cc
@@ -1,4 +1,5 @@
 #include <vector>
+using namespace std;
 
 int islandPerimeter(vector<vector<int>>& grid) {
     int count = 0;
---
+++ LeetCode/657_RobotReturnToOrigin.cc
@@ -0,0 +1,17 @@
+#include <string>
+using namespace std;
+
+bool judgeCircle(string moves) {
+    int x = 0, y = 0;
+    for(int i = 0; i < moves.size(); i++){
+        if(moves[i] == 'U') y++;
+        if(moves[i] == 'D') y--;
+        if(moves[i] == 'L') x--;
+        if(moves[i] == 'R') x++;
+    }
+    return (x == 0 && y == 0);
+}
+
+int main(){
+
+}
