013bdb177980ee123679dcf8ba22be04ca17fa4b
Luke Volpatti <luke.volpatti@gmail.com>
2018-09-05 20:19:49 -0400

    island perimeter

---
+++ LeetCode/463_IslandPerimeter.cc
@@ -0,0 +1,20 @@
+#include <vector>
+
+int islandPerimeter(vector<vector<int>>& grid) {
+    int count = 0;
+    for(int i = 0; i < grid.size(); i++){
+        for(int j = 0; j < grid[0].size(); j++){
+            if(grid[i][j] == 1){
+                if(i == 0 || grid[i - 1][j] == 0) count++;
+                if(j == 0 || grid[i][j - 1] == 0) count++;
+                if(i == grid.size() - 1 || grid[i + 1][j] == 0) count++;
+                if(j == grid[0].size() - 1 || grid[i][j + 1] == 0) count++;
+            }
+        }
+    }
+    return count;
+}
+
+int main(){
+	
+}
