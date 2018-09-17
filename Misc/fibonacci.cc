f1d6d9300475192b418b9734f0264c11b7873af0
Luke Volpatti <luke.volpatti@gmail.com>
2018-09-16 21:40:51 -0400

    bottom up fib

--- Misc/fibonacci.cc
+++ Misc/fibonacci.cc
@@ -20,10 +20,22 @@ int fib2(int n){
     return fib2(n, memo);
 }
 
-
+// Bottom-up dynamic programming
+int fib3(int n){
+    if(n == 0) return 0;
+    int a = 0;
+    int b = 1;
+    for(int i = 1; i < n + 1; i++){
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
+}
 int main(){
     cout << fib1(5) << endl;
     //cout << fib1(50) << endl;
     cout << fib2(5) << endl;
     cout << fib2(5000) << endl;
+    cout << fib3(5) << endl;
 }
