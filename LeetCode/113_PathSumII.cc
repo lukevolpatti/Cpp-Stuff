4e8ccc90397a44ebd184eab345a6272e73a84d74
Luke Volpatti <luke.volpatti@gmail.com>
2018-10-10 10:05:02 -0400

    clean up code

--- LeetCode/113_PathSumII.cc
+++ LeetCode/113_PathSumII.cc
@@ -8,16 +8,10 @@ void path(TreeNode* root, int target, int sum, vector<int>& v, vector<vector<int
     v.push_back(root->val);
     if(!root->left && !root->right && sum == target){
         res.push_back(v);
-        return;
     }
-    if(root->left) {
-        path(root->left, target, sum, v, res);
-        v.pop_back();
-    }
-    if(root->right){
-        path(root->right, target, sum, v, res);
-        v.pop_back();
-    }
+    path(root->left, target, sum, v, res);
+    path(root->right, target, sum, v, res);
+    v.pop_back();
 }
 vector<vector<int>> pathSum(TreeNode* root, int sum) {
     vector<int> v;
