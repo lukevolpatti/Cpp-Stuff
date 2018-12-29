69eb7babf8a19c21942e449c373a9c1428503f4e
Luke Volpatti <luke.volpatti@gmail.com>
2018-12-29 16:21:39 -0500

    c8q11 bottom up dp

--- CTCI/Chapter-8/c8q11.cc
+++ CTCI/Chapter-8/c8q11.cc
@@ -28,11 +28,31 @@ int count_dp(vector<int>& denoms, int amount, int index, vector<vector<int> >& d
     return ways;
 }
 
+// Bottom up dynamic programming
+int count_dp2(vector<int>& denoms, int amount){
+    vector<vector<int> > dp(amount + 1, vector<int> (denoms.size()));
+    for(int i = 0; i < dp.size(); i++){
+        for(int j = 0; j < denoms.size(); j++){
+            if(i - denoms[j] == 0) dp[i][j] = 1;
+            else{
+                int left, above;
+                if(i - denoms[j] < 0) above = 0;
+                else above = dp[i-denoms[j]][j];
+                if(j == 0) left = 0;
+                else left = dp[i][j-1];
+                dp[i][j] = left + above;
+            }
+        }
+    }
+    return dp[dp.size() - 1][dp[0].size() - 1];
+}
+
 // Tested an working on LeetCode
 int main(){
-    int amount = 5;
-    vector<int> denoms = {5,2};
+    int amount = 100;
+    vector<int> denoms = {25, 10, 5, 1};
     cout << count(denoms, amount, 0) << endl;
     vector<vector<int> > dp(amount + 1, vector<int> (denoms.size()));
     cout << count_dp(denoms, amount, 0, dp) << endl;
+    cout << count_dp2(denoms, amount) << endl;
 }
